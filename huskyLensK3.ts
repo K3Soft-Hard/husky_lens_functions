95700e35f00b4950541a8c5fc5539ba0d2ab8f41
K3Soft-Hard <159473672+K3Soft-Hard@users.noreply.github.com>
2024-12-01 17:39:34 +0100

    Update huskyLensK3.ts

--- huskyLensK3.ts
+++ huskyLensK3.ts
@@ -2,10 +2,10 @@
 namespace K3huskyLens {
 
     //%blockId="k3hl_show_coordinates"
-    //%block="show coordinates id: %id"
+    //%block="show coordinates id: %id ||x:%dx y:%dy"
     //%group=Graphs
     //%ledNum.min=0 ledNum.max=4 value.min=0 value.max=5
-    export function idCoordinates(id: number) {
+    export function idCoordinates(id: number = 1, dx: number = 150, dy: number = 30) {
         huskylens.request()
         if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
             huskylens.writeOSD("X:" + huskylens.readeBox(1, Content1.xCenter) + ", Y:" + huskylens.readeBox(1, Content1.yCenter), 150, 30)
