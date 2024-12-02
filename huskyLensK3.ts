90bdfc741fc4ebd67318473afa9b6aaed770c068
K3Soft-Hard <159473672+K3Soft-Hard@users.noreply.github.com>
2024-12-02 11:38:27 +0100

    Update huskyLensK3.ts

--- huskyLensK3.ts
+++ huskyLensK3.ts
@@ -4,15 +4,30 @@ namespace K3huskyLens {
     //%blockId="k3hl_show_coordinates"
     //%block="show coordinates id: %id x:%dx y:%dy"
     //%group=functions
+    //%advanced=true
     /**
      * Show object coordinates on screen, default x = 150, y =30
      */
-    export function idCoordinates(id: number = 1, dx: number = 150, dy: number = 30) {
+    export function showCoordinates(id: number = 1, dx: number = 150, dy: number = 30) {
         huskylens.request()
         if (huskylens.isAppear(id, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
             huskylens.writeOSD("X:" + huskylens.readeBox(id, Content1.xCenter) + ", Y:" + huskylens.readeBox(id, Content1.yCenter), dx, dy)
         } else {
             huskylens.clearOSD()
+        }
+    }
+    //%blockId="k3hl_get_coordinates"
+    //%block="get coordinates id: %id"
+    //%group=functions
+    /**
+     * Return object coordinates
+     */
+    export function getCoordinates(id: number = 1, dx: number = 150, dy: number = 30) {
+        huskylens.request()
+        if (huskylens.isAppear(id, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            return ("X:" + huskylens.readeBox(id, Content1.xCenter) + ", Y:" + huskylens.readeBox(id, Content1.yCenter),)
+        } else {
+            return ()
         }
     }
 }
